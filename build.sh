365d689e2fa7e289199ed7c821f716bbc42169f5
Jacob Carlborg <jacob.carlborg@tretti.se>
2011-10-10 09:47:32 +0200

    Changed "orange" from a symlink to a submodule.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rdmd --build-only -oforb -L-lruby.1.9.1-static -J./resources -L-lz "$@" orbit/orb/Orb.d
+rdmd --build-only -oforb -Iorange -L-lruby.1.9.1-static -J./resources -L-lz "$@" orbit/orb/Orb.d

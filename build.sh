31d2de094935033e329b98af0567688f53bcb7d5
Jacob Carlborg <doob@me.com>
2013-01-13 17:14:40 +0100

    Remove orange and add mambo as submodule.

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-rdmd --build-only -ofbin/orb -m32 -Iorange -L-lruby.1.9.1-static -J./resources -L-ltango -L-lz "$@" orbit/orb/Orb.d
+if [ -s "$HOME/.dvm/scripts/dvm" ] ; then
+    . "$HOME/.dvm/scripts/dvm" ;
+    dvm use 2.061
+fi
+
+rdmd --build-only -ofbin/orb -m32 -Imambo -L-lruby.1.9.1-static -J./resources -L-ltango -L-lz "$@" orbit/orb/Orb.d
